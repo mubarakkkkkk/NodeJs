f9762602ae12ef49f7b7964302c9eb3ea69cf7fa
mubarakkkkkk <abiolamubarakolamide05@gmail.com>
2026-05-05 12:19:12 +0100

    chapter 3 completed

--- Chapter3/index.js
+++ Chapter3/index.js
@@ -1,1 +1,8 @@
-console.log('testing')
+const { format } = require('date-fns')
+const {v4: uuid} = require('uuid')
+
+console.log(format(new Date(), 'yyyy-MM-dd/HH:mm:ss'))
+
+console.log(uuid())
+
+console.log
